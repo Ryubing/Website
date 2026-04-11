f7c1961e15e86530a3199d3b2930ba316fed2ec0
GreemDev <greemdev@ryujinx.app>
2026-04-11 18:03:50 -0500

    Update consts.ts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -6,5 +6,5 @@ export default class Consts {
     static readonly LINUXARM_URL = "https://update.ryujinx.app/download/query?os=linux&arch=arm64&rc=stable" as const;
     static readonly APPIMAGEARM_URL = "https://update.ryujinx.app/download/query?os=linuxai&arch=arm64&rc=stable" as const;
 
-    static readonly FORGEJO_HOST = "git.greemdev.net"
+    static readonly FORGEJO_HOST = "git.ryujinx.app" as const;
 }
